ca8005387925fc7c4354bd6bbda314e8278ec470
Filip Žitný <filipzitny@gmail.com>
2024-01-08 02:46:17 +0100

    Task two rewok

--- Task_2/inv_prime_sum.m
+++ Task_2/inv_prime_sum.m
@@ -1,22 +1,6 @@
 function sum_inv_primes = INV_PRIME_SUM(n)
-    % INV_PRIME_SUM calculates the sum of the reciprocals of the first n prime numbers
-
-    % Find the first n prime numbers
-    prime_numbers = primes(nthprime(n));
-
-    % Calculate the sum of their reciprocals
-    sum_inv_primes = sum(1 ./ prime_numbers);
-end
-
-function p = nthprime(n)
-    % Helper function to find the nth prime number
-    p = 2; % Starting from the first prime number
-    count = 1;
-    while count < n
-        p = p + 1;
-        if isprime(p)
-            count = count + 1;
-        end
-    end
+    primes_array = primes(nthprime(n));
+    
+    sum_inv_primes = sum(1 ./ primes_array);
 end
 
--- Task_2/plot_inv_prime_sum.m
+++ Task_2/plot_inv_prime_sum.m
@@ -1,17 +1,15 @@
 function PLOT_INV_PRIME_SUM(nmax)
-    % Initialize an array to store the partial sums
-    partial_sums = zeros(1, nmax);
-    
-    % Calculate partial sums
-    for i = 1:nmax
-        partial_sums(i) = INV_PRIME_SUM(i);
+
+    sums = zeros(1, nmax);
+
+    for n = 1:nmax
+        sums(n) = INV_PRIME_SUM(nthprime(n));
     end
-    
-    % Plot the results using a log-log plot
-    loglog(1:nmax, partial_sums);
-    xlabel('n');
-    ylabel('Sum of Reciprocals of First n Prime Numbers');
-    title('Log-Log Plot of Partial Sums of Reciprocal Prime Numbers');
+
+    loglog(1:nmax, sums, '-o');
+    title('Sum of Inverse of First N Prime Numbers');
+    xlabel('Number of Prime Numbers (N)');
+    ylabel('Sum of Inverse of Prime Numbers');
     grid on;
 end
 
