1d30f8af776b8c4611204973914de1021dc5f7c7
Filip Žitný <filipzitny@gmail.com>
2024-01-08 02:02:57 +0100

    task one rework

--- Task_1/CTYRSTEN.m
+++ Task_1/CTYRSTEN.m
@@ -1,10 +1,14 @@
-function isValid = CTYRSTEN(a, b, c, d, e, f)
+function exists = CTYRSTEN(a, b, c, d, e, f)
 
-    if (a + b + c > d) && (a + b + d > c) && (a + c + d > b) && (b + c + d > a)
-
-        isValid = true;
+    if (a + b + c > d) && (b + c + d > a) && (c + d + a > b) && (d + a + b > c) && ...
+       (a + b + e > f) && (b + e + f > a) && (e + f + a > b) && (f + a + b > e) && ...
+       (a + c + e > f) && (c + e + f > a) && (e + f + a > c) && (f + a + c > e) && ...
+       (b + c + e > f) && (c + e + f > b) && (e + f + b > c) && (f + b + c > e) && ...
+       (a + d + e > f) && (d + e + f > a) && (e + f + a > d) && (f + a + d > e) && ...
+       (b + d + e > f) && (d + e + f > b) && (e + f + b > d) && (f + b + d > e)
+        exists = true;
     else
-        isValid = false;
+        exists = false;
     end
 end
 
