9485a9ed3a8d1c4b135d7c54fc1c1a7f036be604
Filip Žitný <filipzitny@gmail.com>
2024-01-08 02:56:23 +0100

    update test

--- Task_2/test.m
+++ Task_2/test.m
@@ -1,28 +1,14 @@
-% Test cases for INV_PRIME_SUM
-tests = [2, 3, 5, 10]; % Example test cases
-expected_results = [1/2 + 1/3, 1/2 + 1/3 + 1/5, 1/2 + 1/3 + 1/5 + 1/7, 1/2 + 1/3 + 1/5 + 1/7 + 1/11]; 
-
-% Run the tests for INV_PRIME_SUM
-for i = 1:length(tests)
-    actual_result = INV_PRIME_SUM(tests(i));
-    if abs(actual_result - expected_results(i)) < 1e-6 % Allowing a small error margin
-        fprintf('Test %d for INV_PRIME_SUM passed\n', i);
-    else
-        fprintf('Test %d for INV_PRIME_SUM failed. Expected %f but got %f\n', i, expected_results(i), actual_result);
-    end
-end
-
-% Assuming PLOT_INV_PRIME_SUM is defined elsewhere or in the MATLAB path
-% Test calls for PLOT_INV_PRIME_SUM
-test_values = [10, 50, 100]; % Example test cases
-
-% Run the tests for PLOT_INV_PRIME_SUM
+% Test for INV_PRIME_SUM
+disp('Testing INV_PRIME_SUM Function');
+test_values = [1, 5, 10, 20];
 for i = 1:length(test_values)
-    try
-        PLOT_INV_PRIME_SUM(test_values(i));
-        fprintf('Test with nmax = %d for PLOT_INV_PRIME_SUM passed (check plot visually)\n', test_values(i));
-    catch e
-        fprintf('Test with nmax = %d for PLOT_INV_PRIME_SUM failed due to an error: %s\n', test_values(i), e.message);
-    end
+    sum_inv = INV_PRIME_SUM(test_values(i));
+    disp(['Sum of inverses of first ', num2str(test_values(i)), ' primes: ', num2str(sum_inv)]);
 end
+
+% Test for PLOT_INV_PRIME_SUM
+disp('Testing PLOT_INV_PRIME_SUM Function');
+nmax = 10; 
+PLOT_INV_PRIME_SUM(nmax);
+disp(['PLOT_INV_PRIME_SUM plot generated for nmax = ', num2str(nmax)]);
 
